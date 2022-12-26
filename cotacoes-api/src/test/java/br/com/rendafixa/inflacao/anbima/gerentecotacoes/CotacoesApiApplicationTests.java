84057ff74dfa729007cf3bc923e4eac6e9d941fd
Jonathas Mariano <jonathas.mariano@outlook.com>
2022-12-26 18:01:19 -0300

    Ajuste para o multiproject

---
+++ cotacoes-api/src/test/java/br/com/rendafixa/inflacao/anbima/gerentecotacoes/CotacoesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rendafixa.inflacao.anbima.gerentecotacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CotacoesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
